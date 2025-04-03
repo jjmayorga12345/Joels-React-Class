7ed32e5388812f25e64b9c8ca3b8b67a836878c8
Joel <139776060+jjmayorga12345@users.noreply.github.com>
2025-04-02 21:08:23 -0400

    Homework

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,15 +1,16 @@
 import './App.css';
-import Todo from './Todo'
-import { Modal, Joel } from "./Modal"
+import Todo from './Todo';
+import { Modal, Joel } from "./Modal";
+
 function App() {
   return (
     <div class="todo-container">
       <div class="card">
-        <Todo></Todo>
-
-        </div>
+        <Todo title="Learn React"></Todo>
+        <Todo title="Practice React"></Todo>
+        <Todo title="Learn React part 2"></Todo>
       </div>
-
+    </div>
   );
 }
 
